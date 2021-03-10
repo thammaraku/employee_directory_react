88df0d03cd4a57ab8a6515c5f5e89a0f7afd97f6
thamma <thammaraku@gmail.com>
2021-03-10 00:06:10 -0600

    add search function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Main from "./components/Main";
-import EmployeeList from "./components/Employee_list";
 import "./components/style.css";
 
 function App() {
